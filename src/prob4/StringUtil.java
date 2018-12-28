a21742a2a9baba4b95729c539d5a1d39fb572ca9
BIT <BIT@DESKTOP-E23PFRN>
2018-12-28 19:06:40 +0900

    181228 19:16 Prob4 보충

--- src/prob4/StringUtil.java
+++ src/prob4/StringUtil.java
@@ -2,13 +2,16 @@ package prob4;
 
 public class StringUtil {
 	public static String concatenate(String[] str) {
-        //문자열을 결합하여 리턴 하는 메소드 구현
+		//문자열을 결합하여 리턴 하는 메소드 구현
 		String resultStr = "";
 		for(int i=0;i<str.length;i++) {
 			resultStr += str[i];
 		}
+
+		/* for(String s : str) { resultStr += s; } */
+
 		return resultStr;
-		
-    }
+
+	}
 
 }
