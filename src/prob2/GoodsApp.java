04eed8435f5774f288afee4e82edb311be4456af
BIT <BIT@DESKTOP-E23PFRN>
2018-12-28 16:37:53 +0900

    181228 16:37 Prob1 Complete

--- src/prob2/GoodsApp.java
+++ src/prob2/GoodsApp.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 
 public class GoodsApp {
 	private static final int COUNT_GOODS = 3;
-
+	//final을 변수에서 넣어주면 더이상 바꿀수 없다. final을 이용하여 변수처럼 사용한다.
 	public static void main(String[] args) {
-
+		
+		Goods[] arrayGoods = new Goods[COUNT_GOODS];
+		Scanner scanner = new Scanner(System.in);
+		for(int i=0;i<COUNT_GOODS;i++) {
+			String line = scanner.nextLine();
+			String[] tokens = line.split(" ");
+			
+			String name = tokens[0];
+			int price = Integer.parseInt(tokens[1]);
+			
+		}
 	}
 }
